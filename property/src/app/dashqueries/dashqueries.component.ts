4663f83ed4f03aebff563b18d30408cdb76f9931
Akila Sri M <akil3016@global.chainsys.com>
2022-06-06 12:16:59 +0530

    sonarcloud1

--- property/src/app/dashqueries/dashqueries.component.ts
+++ property/src/app/dashqueries/dashqueries.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder, Validators, NgForm } from '@angular/forms';
+import { FormBuilder} from '@angular/forms';
 import { ApiserviceService } from '../apiservice.service';
 import { Router } from '@angular/router';
 
